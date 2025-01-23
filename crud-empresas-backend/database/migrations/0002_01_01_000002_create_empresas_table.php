aaba7832ceae2a123ee11fb2f0ce188658eabb03
Schmxdt <airtonschmidt2018@gmail.com>
2025-01-23 17:36:42 -0300

    feat: migration for empresa

---
+++ crud-empresas-backend/database/migrations/0002_01_01_000002_create_empresas_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('email')->unique(); 
+            $table->string('endereco');
+            $table->string('telefone')->nullable();
+            $table->string('site')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('empresas');
+    }
+};
